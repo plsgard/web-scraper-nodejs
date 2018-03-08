b7740cb0cfcb12995bd7240a9661ee44ccf8f385
Pier-Lionel Sgard <hello@plsgd.com>
2018-03-08 16:30:17 +0100

    Configuration pour déploiement externe

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ class Scrap {
     return sortByKey(obj, "votesCount");
   }
 
-  async scrapProjects(event) {
+  async scrapProjects() {
     var i = this.min;
     var that = this;
     for (var i = that.min; i <= that.max; i++) {
@@ -47,7 +47,6 @@ class Scrap {
       if (result.project != "") {
         that.projects.push(result);
       }
-      //event(i, that.max);
     }
     return that.projects;
   }
@@ -100,53 +99,41 @@ var http = require("http");
 
 var server = http.createServer(function(request, response) {
   var path = url.parse(request.url).pathname;
-  console.log(path);
   if (path == "/stats") {
-    console.log("request received");
     request.setTimeout(maxProject * 500);
     response.writeHead(200, { "Content-Type": "text/html" });
-    // response.writeHead(200, { "Content-Length": 20 });
     var scrap = new Scrap();
     var that = this;
-    scrap
-      .scrapProjects(/*(current, max) => {
-        console.log("emit : " + current);
-        that.emit("progress", {
-          lengthComputable: true,
-          loaded: current,
-          total: max
-        });
-      }*/)
-      .then(res => {
-        var results = sortByKey(res, "votesCount");
-        var htmlResult =
-          "<table class='table'><thead class='thead-dark'><tr><th scope='col'>#</th><th scope='col'>Vote</th><th scope='col'>Project</th><th scope='col'>Company</th><th scope='col'>Category</th><th scope='col'><small>Date: " +
-          getCurrentDate() +
-          "</small></th></tr></thead>";
-        for (var i = 0; i < results.length; i++) {
-          var orga = results[i].organisation.toLowerCase();
-          htmlResult +=
-            "<tr class='" +
-            (orga == "courseur" || orga == "jeanne rives"
-              ? "table-danger"
-              : i < 140 ? "table-success" : "") +
-            "'><th scope='row'>" +
-            (i + 1) +
-            "</th><td>" +
-            results[i].votesCount +
-            "</td><td>" +
-            results[i].project +
-            "</td><td>" +
-            results[i].organisation +
-            "</td><td>" +
-            results[i].category +
-            "</td><td><a href='" +
-            results[i].url +
-            "' target='_blank' class='btn btn-light'>Open</a></td></tr>";
-        }
+    scrap.scrapProjects().then(res => {
+      var results = sortByKey(res, "votesCount");
+      var htmlResult =
+        "<table class='table'><thead class='thead-dark'><tr><th scope='col'>#</th><th scope='col'>Vote</th><th scope='col'>Project</th><th scope='col'>Company</th><th scope='col'>Category</th><th scope='col'><small>Date: " +
+        getCurrentDate() +
+        "</small></th></tr></thead>";
+      for (var i = 0; i < results.length; i++) {
+        var orga = results[i].organisation.toLowerCase();
+        htmlResult +=
+          "<tr class='" +
+          (orga == "courseur" || orga == "jeanne rives"
+            ? "table-danger"
+            : i < 140 ? "table-success" : "") +
+          "'><th scope='row'>" +
+          (i + 1) +
+          "</th><td>" +
+          results[i].votesCount +
+          "</td><td>" +
+          results[i].project +
+          "</td><td>" +
+          results[i].organisation +
+          "</td><td>" +
+          results[i].category +
+          "</td><td><a href='" +
+          results[i].url +
+          "' target='_blank' class='btn btn-light'>Open</a></td></tr>";
+      }
 
-        response.end(htmlResult, "utf-8");
-      });
+      response.end(htmlResult, "utf-8");
+    });
   } else {
     fs.readFile("./index.html", function(err, file) {
       if (err) {
@@ -161,5 +148,3 @@ var server = http.createServer(function(request, response) {
 
 var port = process.env.PORT || 1337;
 server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
